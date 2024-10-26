8ce9bcfab7663d419c4dbcec45faba9404c3f63b
Marwan <marwanmo475@gmail.com>
2024-10-25 21:19:34 -0600

    updated boot loader and documented code

--- Bootloader/Bootloader.asm
+++ Bootloader/Bootloader.asm
@@ -1,26 +1,23 @@
-bits 16
-org 0x7c00 
+bits 16 ; starting in 16 bit mode
+org 0x7c00 ; bios loads bootloader to this address, setting offset
 
-boot:
-    mov si,hello
-    mov ah,0x0e
+mov si, 0 ; counter for print function
 
-.loop:
-    lodsb
-    or al,al
-    jz halt
-    int 0x10
-    jmp .loop
+print: ; printing function, prints a char using interupt 
+    mov ah, 0x0e ; setting interupt code
+    mov al, [hello + si] ; getting char from memory
+    int 0x10 ; calling interupt
+    add si, 1 ; moving to next char, incrementing counter
+    cmp byte [hello + si], 0 ; checking if at end of string
+    jne print ; loops untill all chars are printed
 
-halt:
-    cli
-    hlt
+jmp $ ; infinte loop after code is done
 
-hello: db "Hello world!",0
+hello: ; string to be printed
+    db "Welcome to TwistedOS",0
 
-
-times 510 - ($-$$) db 0
-dw 0xaa55
+times 510 - ($-$$) db 0 ; fills rest of code with 0s
+dw 0xaa55 ;magic number for boot loader
 
 
 
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # TwistedOS
+
+https://www.ctyme.com/intr/rb-0106.htm // interupt codes
