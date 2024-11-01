62de0553a0bb982df12bed66de2ff02baa2b484a
Marwan <marwanmo475@gmail.com>
2024-11-01 01:09:30 -0600

    set up main loading loop for stage 2

--- Bootloader/Stage2/stage2.c
+++ Bootloader/Stage2/stage2.c
@@ -6,11 +6,18 @@ void stage2(int LBA){
 
   void* buffer = (void*)0xBFFF;
 
-  int res = ReadFromDisk((uint8)0,LBA,(uint8)1,buffer);
+  int res = 1;
+  while(res && LBA){
+    res = ReadFromDisk((uint8)0,LBA,(uint8)1,buffer);
 
-  kernelprint("Worked %d%n",res);
+    kernelprint("Disk Read Worked %d%n",res);
 
-  res = FindNextCluster(LBA);
+    LBA = FindNextCluster(LBA);
 
-  kernelprint("Worked 2 %d",res);
+    kernelprint("Cluster Found %d%n",LBA);
+
+    console();
+  }
+
+  kernelprint("DONE");
 }
