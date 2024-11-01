0b98753a1d6f02a8c3437fb192106a4235a95fe5
Marwan <marwanmo475@gmail.com>
2024-11-01 02:21:08 -0600

    update memory.h to fit with new kernel load area

--- Kernel/memory/memory.h
+++ Kernel/memory/memory.h
@@ -4,9 +4,9 @@
 #include "../headers/kernel.h"
 #include "../headers/utility.h"
 
-#define HEAP_MEMORY 0x100000
-#define BLOCK_SIZE 1
-#define BLOCKS 40000
+#define HEAP_MEMORY 0x25F900 // 1.44mb higher then kernel load adress
+#define BLOCK_SIZE 16 // bytes per block
+#define BLOCKS 4000 // total amount of blocks availble for allocation
 
 void kernelmemset(uint8* buf,int count,uint8 set);
 void heapinit();
